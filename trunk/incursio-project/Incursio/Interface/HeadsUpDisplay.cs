0614b4791fb1a4abd8fc774070732277368068f4
armstrhb <armstrhb@8aa576c9-6a45-0410-b38e-211266189506>
2008-03-26 16:16:11 +0000

    Updated HeadsUpDisplay to conform better with recent changes.  Camp and GuardTower portraits added for selection.  For Camp:  Current Build Information available.  This includes both "hay i'm buildn diz rite here" string and also a progress bar.

--- trunk/incursio-project/Incursio/Interface/HeadsUpDisplay.cs
+++ trunk/incursio-project/Incursio/Interface/HeadsUpDisplay.cs
@@ -12,7 +12,7 @@ namespace Incursio.Classes
 {
   public class HeadsUpDisplay
     {
-        //Texture2D containing the utility bar.
+        /*//Texture2D containing the utility bar.
         Texture2D utilityBar; 
 
         //Texture2D containing the resource bar
@@ -24,7 +24,7 @@ namespace Incursio.Classes
 
         //icons
         Texture2D lightInfantryIcon;
-        Texture2D archerIcon;
+        Texture2D archerIcon;*/
 
         //helper variables for determining selection
         private int barX;
@@ -35,7 +35,7 @@ namespace Incursio.Classes
         /// loadHeadsUpDisplay loads the HUD content into the game.  from here it can be displayed on the screen.
         /// note:  the HUD will only be displayed when the gamestate is in scenario play.
         /// </summary>
-        public void loadHeadsUpDisplay(Texture2D bar, Texture2D lightInfantry, Texture2D archer, Texture2D infIcon, Texture2D archIcon, Texture2D resourceBar)
+        /*public void loadHeadsUpDisplay(Texture2D bar, Texture2D lightInfantry, Texture2D archer, Texture2D infIcon, Texture2D archIcon, Texture2D resourceBar)
         {
             // Load the images for the UI
             utilityBar = bar;
@@ -46,6 +46,12 @@ namespace Incursio.Classes
             barX = 0;
             numUnits = 0;
             this.resourceBar = resourceBar;
+        }*/
+
+        public void loadHeadsUpDisplay()
+        {
+            barX = 0;
+            numUnits = 0;
         }
 
         public List<BaseGameEntity> update(Cursor cursor)
@@ -91,33 +97,83 @@ namespace Incursio.Classes
             int numUnitsSelected = selectedUnits.Count;
 
             // draw the bars
-            spriteBatch.Draw(utilityBar, new Rectangle(0, height - utilityBar.Height, utilityBar.Width, utilityBar.Height), Color.White);
-            spriteBatch.Draw(resourceBar, new Rectangle(1024 - resourceBar.Width, 0, resourceBar.Width, resourceBar.Height), Color.White);
+            spriteBatch.Draw(TextureBank.InterfaceTextures.headsUpDisplay, new Rectangle(0, height - TextureBank.InterfaceTextures.headsUpDisplay.Height, TextureBank.InterfaceTextures.headsUpDisplay.Width, TextureBank.InterfaceTextures.headsUpDisplay.Height), Color.White);
+            spriteBatch.Draw(TextureBank.InterfaceTextures.resourceDisplay, new Rectangle(1024 - TextureBank.InterfaceTextures.resourceDisplay.Width, 0, TextureBank.InterfaceTextures.resourceDisplay.Width, TextureBank.InterfaceTextures.resourceDisplay.Height), Color.White);
 
             //for the selected unit portrait
             if (numUnitsSelected != 0)
             {
                 if (selectedUnits[0].getType() == State.EntityName.LightInfantry)
                 {
-                    spriteBatch.Draw(lightInfantryPortrait, new Rectangle(241, height - 129, lightInfantryPortrait.Width, lightInfantryPortrait.Height), Color.White);
+                    spriteBatch.Draw(TextureBank.InterfaceTextures.lightInfantryPortrait, new Rectangle(241, height - 129, TextureBank.InterfaceTextures.lightInfantryPortrait.Width, TextureBank.InterfaceTextures.lightInfantryPortrait.Height), Color.White);
                 }
                 else if (selectedUnits[0].getType() == State.EntityName.Archer)
                 {
-                    spriteBatch.Draw(archerPortrait, new Rectangle(241, height - 129, archerPortrait.Width, archerPortrait.Height), Color.White);
+                    spriteBatch.Draw(TextureBank.InterfaceTextures.archerPortrait, new Rectangle(241, height - 129, TextureBank.InterfaceTextures.archerPortrait.Width, TextureBank.InterfaceTextures.archerPortrait.Height), Color.White);
+                }
+                else if (selectedUnits[0].getType() == State.EntityName.Camp)
+                {
+                    spriteBatch.Draw(TextureBank.InterfaceTextures.basePortrait, new Rectangle(241, height - 129, TextureBank.InterfaceTextures.basePortrait.Width, TextureBank.InterfaceTextures.basePortrait.Height), Color.White);
+                }
+                else if (selectedUnits[0].getType() == State.EntityName.GuardTower)
+                {
+                    spriteBatch.Draw(TextureBank.InterfaceTextures.guardTowerPortrait, new Rectangle(241, height - 129, TextureBank.InterfaceTextures.guardTowerPortrait.Width, TextureBank.InterfaceTextures.guardTowerPortrait.Height), Color.White);
                 }
             }
 
             //unit attributes
             if (numUnitsSelected == 1)
             {
-                //commented out selectedUnit[].getType returns enum not string
-                //spriteBatch.DrawString(font, selectedUnits[0].getType(), new Vector2(572, height - 118), Color.White, 0, font.MeasureString(selectedUnits[0].getType()) / 2, 1.0f, SpriteEffects.None, 0.5f);
+                //for the unit's display name
+                if (selectedUnits[0].getType() == State.EntityName.LightInfantry)
+                {
+                    spriteBatch.DrawString(font, "Light Infantry", new Vector2(572, height - 118), Color.White, 0, font.MeasureString("Light Infantry") / 2, 1.0f, SpriteEffects.None, 0.5f);
+                }
+                else if (selectedUnits[0].getType() == State.EntityName.Archer)
+                {
+                    spriteBatch.DrawString(font, "Archer", new Vector2(572, height - 118), Color.White, 0, font.MeasureString("Archer") / 2, 1.0f, SpriteEffects.None, 0.5f);
+                }
+                else if (selectedUnits[0].getType() == State.EntityName.HeavyInfantry)
+                {
+                    spriteBatch.DrawString(font, "Heavy Infantry", new Vector2(572, height - 118), Color.White, 0, font.MeasureString("Heavy Infantry") / 2, 1.0f, SpriteEffects.None, 0.5f);
+                }
+                else if (selectedUnits[0].getType() == State.EntityName.Hero)
+                {
+                    spriteBatch.DrawString(font, "Hero", new Vector2(572, height - 118), Color.White, 0, font.MeasureString("Hero") / 2, 1.0f, SpriteEffects.None, 0.5f);
+                }
+                else if (selectedUnits[0].getType() == State.EntityName.ControlPoint)
+                {
+                    spriteBatch.DrawString(font, "Control Point", new Vector2(572, height - 118), Color.White, 0, font.MeasureString("Control Point") / 2, 1.0f, SpriteEffects.None, 0.5f);
+                }
+                else if (selectedUnits[0].getType() == State.EntityName.Camp)
+                {
+                    spriteBatch.DrawString(font, "Camp", new Vector2(572, height - 118), Color.White, 0, font.MeasureString("Camp") / 2, 1.0f, SpriteEffects.None, 0.5f);
+                }
+                else if (selectedUnits[0].getType() == State.EntityName.GuardTower)
+                {
+                    spriteBatch.DrawString(font, "Guard Tower", new Vector2(572, height - 118), Color.White, 0, font.MeasureString("Guard Tower") / 2, 1.0f, SpriteEffects.None, 0.5f);
+                }
 
+                //stats!
                 spriteBatch.DrawString(font, "Health: " + selectedUnits[0].getHealth(), new Vector2(572, height - 90), Color.White, 0, font.MeasureString("Health: XXX") / 2, 1.0f, SpriteEffects.None, 0.5f);
 
                 if(selectedUnits[0].getType() == State.EntityName.Archer || selectedUnits[0].getType() == State.EntityName.HeavyInfantry ||
                    selectedUnits[0].getType() == State.EntityName.Hero   || selectedUnits[0].getType() == State.EntityName.LightInfantry)
                 spriteBatch.DrawString(font, "Attack: " + (selectedUnits[0] as Unit).getDamage() + "   Armor: " + (selectedUnits[0] as Unit).getArmor(), new Vector2(572, height - 65), Color.White, 0, font.MeasureString("Attack: XXX   Armor: XXX") / 2, 1.0f, SpriteEffects.None, 0.5f);
+
+                if (selectedUnits[0].getType() == State.EntityName.Camp)
+                {
+                    double buildRatio = (selectedUnits[0] as CampStructure).getPercentDone();
+
+                    //lets the player know what we're building, and the progress on that construction
+                    if ((selectedUnits[0] as CampStructure).isBuilding())
+                    {
+                        spriteBatch.DrawString(font, "Building:  " + (selectedUnits[0] as CampStructure).getCurrentlyBuilding(), new Vector2(572, height - 65), Color.White, 0, font.MeasureString("Building:  XXXXXXXXXXXXXXX") / 2, 1.0f, SpriteEffects.None, 0.5f);
+                        spriteBatch.Draw(TextureBank.EntityTextures.healthRatioTexture, new Rectangle(462, height - 50, TextureBank.EntityTextures.healthRatioTexture.Width * 5, TextureBank.EntityTextures.healthRatioTexture.Height * 3), Color.Black);
+                        spriteBatch.Draw(TextureBank.EntityTextures.healthRatioTexture, new Rectangle(462, height - 50, (int) (TextureBank.EntityTextures.healthRatioTexture.Width * 5 * buildRatio) , TextureBank.EntityTextures.healthRatioTexture.Height * 3), Color.Lime);
+                    }
+                    
+                }
             }
             else if (numUnitsSelected > 1)
             {
@@ -126,24 +182,24 @@ namespace Incursio.Classes
 
                     if (i <= 5)
                     {
-                        if (selectedUnits[i].getType().Equals("Light Infantry"))
+                        if (selectedUnits[i].getType() == State.EntityName.LightInfantry)
                         {
-                            spriteBatch.Draw(lightInfantryIcon, new Rectangle(383 + i * 60, height - 129, lightInfantryIcon.Width, lightInfantryIcon.Height), Color.White);
+                            spriteBatch.Draw(TextureBank.InterfaceTextures.lightInfantryIcon, new Rectangle(383 + i * 60, height - 129, TextureBank.InterfaceTextures.lightInfantryIcon.Width, TextureBank.InterfaceTextures.lightInfantryIcon.Height), Color.White);
                         }
-                        else if (selectedUnits[i].getType().Equals("Archer"))
+                        else if (selectedUnits[i].getType() == State.EntityName.Archer)
                         {
-                            spriteBatch.Draw(archerIcon, new Rectangle(383 + i * 60, height - 129, archerIcon.Width, archerIcon.Height), Color.White);
+                            spriteBatch.Draw(TextureBank.InterfaceTextures.archerIcon, new Rectangle(383 + i * 60, height - 129, TextureBank.InterfaceTextures.archerIcon.Width, TextureBank.InterfaceTextures.archerIcon.Height), Color.White);
                         }
                     }
                     else
                     {
-                        if (selectedUnits[i].getType().Equals("Light Infantry"))
+                        if (selectedUnits[i].getType() == State.EntityName.LightInfantry)
                         {
-                            spriteBatch.Draw(lightInfantryIcon, new Rectangle(383 + i * 60 - 6*60, height - 84, lightInfantryIcon.Width, lightInfantryIcon.Height), Color.White);
+                            spriteBatch.Draw(TextureBank.InterfaceTextures.lightInfantryIcon, new Rectangle(383 + i * 60 - 6 * 60, height - 84, TextureBank.InterfaceTextures.lightInfantryIcon.Width, TextureBank.InterfaceTextures.lightInfantryIcon.Height), Color.White);
                         }
-                        else if (selectedUnits[i].getType().Equals("Archer"))
+                        else if (selectedUnits[i].getType() == State.EntityName.Archer)
                         {
-                            spriteBatch.Draw(archerIcon, new Rectangle(383 + i * 60 - 6*60, height - 84, archerIcon.Width, archerIcon.Height), Color.White);
+                            spriteBatch.Draw(TextureBank.InterfaceTextures.archerIcon, new Rectangle(383 + i * 60 - 6 * 60, height - 84, TextureBank.InterfaceTextures.archerIcon.Width, TextureBank.InterfaceTextures.archerIcon.Height), Color.White);
                         }
                     }
 
