a5ddcc6a47ca5f109171895e118b8ce9ade58b66
mitch.mgtzx <mitch.mgtzx@8aa576c9-6a45-0410-b38e-211266189506>
2008-04-25 18:50:05 +0000

    hopefully reverted to include changes I accidentally removed!!!!omg

--- incursio-project/Incursio/Classes/BaseMapEntity.cs
+++ incursio-project/Incursio/Classes/BaseMapEntity.cs
@@ -9,20 +9,34 @@ using Incursio.Utils;
 
 namespace Incursio.Classes
 {
-  public class BaseMapEntity
+    public class BaseMapEntity
     {
         public Coordinate location;
         public Boolean passable = true;
 
         public Texture2D texture;
 
-        public BaseMapEntity(){
+        public BaseMapEntity()
+        {
 
         }
 
-        public BaseMapEntity(Texture2D tex)
+        public BaseMapEntity(Texture2D tex, bool passable, int x, int y)
         {
             this.texture = tex;
+            this.passable = passable;
+            this.location = new Coordinate(x, y);
+        }
+
+        public virtual void setOccupancy(ref byte[,] grid)
+        {
+            grid[location.x, location.y] = (byte)(passable ? 1 : 0);
+        }
+
+        public virtual void addToMap(MapBase map)
+        {
+            map.addMapEntity(this, location.x, location.y);
+            this.setOccupancy(ref map.occupancyGrid);
         }
     }
 }
