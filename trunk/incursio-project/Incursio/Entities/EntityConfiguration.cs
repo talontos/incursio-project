3b00917da9d14e91e9f6f0e13370f4f317148447
mitch.mgtzx <mitch.mgtzx@8aa576c9-6a45-0410-b38e-211266189506>
2008-05-13 18:39:42 +0000

    More component stuff

--- trunk/incursio-project/Incursio/Entities/EntityConfiguration.cs
+++ trunk/incursio-project/Incursio/Entities/EntityConfiguration.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Incursio.Entities.Components;
 
 namespace Incursio.Entities
 {
@@ -11,20 +12,43 @@ namespace Incursio.Entities
     {
         public int classID;
         public string className;
-        public List<KeyValuePair<string, uint>> attributes;
+
+        public int keyId = -1;
+        public int maxHealth = 100;
+        public int health = 100;
+        public int armor = 0;
+        public int sightRange = 0;
+        public Coordinate location = new Coordinate(0, 0);
+        public int pointValue = 0;
+        public int playerId = -1;
+
+        public List<ComponentConfiguration> components = new List<ComponentConfiguration>();
 
         public EntityConfiguration(int id, string name, params string[] args){
             this.classID = id;
             this.className = name;
 
-            //args?
+            //set all other properties that are given
+            for(int i = 0; (i + 1) < args.Length; i += 2){
+                switch(args[i]){
+                    case "maxHealth":   maxHealth   = args[i + 1]; break;
+                    case "health":      health      = args[i + 1]; break;
+                    case "armor":       armor       = args[i + 1]; break;
+                    case "sightRange":  sightRange  = args[i + 1]; break;
+                    case "pointValue":  pointValue  = args[i + 1]; break;
+                    case "playerId":    playerId    = args[i + 1]; break;
+                    default: break;
+                }
+            }
         }
 
-        public void addAttrubute(string name, uint value){
-            KeyValuePair<string, uint> newPair = new KeyValuePair<string, uint>(name, value);
+        private void addComponentConfiguration(string componentType, params string[] args){
+            ComponentConfiguration cc = new ComponentConfiguration(componentType);
 
-            if(!attributes.Contains(newPair))
-                attributes.Add(newPair);            
+            for (int i = 0; (i + 1) < args.Length; i += 2){
+                cc.addAttribute(new KeyValuePair<string, object>(args[i], args[i + 1]));
+            }
         }
+
     }
 }
