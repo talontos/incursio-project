cc2289e8f7c94de9d1f5f0440c77486c8f55abe9
mitch.mgtzx <mitch.mgtzx@8aa576c9-6a45-0410-b38e-211266189506>
2008-06-27 19:20:07 +0000

    Some error-handling (we need more of this) Added 'Simpleland' - very simple "mod"

--- incursio-project/Incursio/Utils/Util.cs
+++ incursio-project/Incursio/Utils/Util.cs
@@ -25,7 +25,10 @@ namespace Incursio.Utils
         }
 
         public static int selectRandomInt(ref List<int> list){
-            return list[Incursio.rand.Next(0, list.Count)];
+            //if (list.Count > 0)
+                return list[Incursio.rand.Next(0, list.Count)];
+            //else
+            //    return -1;
         }
     }
 }
