b790d58b26244149ae42c14f3b141e6d5f852a91
mitch.mgtzx <mitch.mgtzx@8aa576c9-6a45-0410-b38e-211266189506>
2008-08-06 12:57:38 +0000

    Okay; moved AI (hopefully) to correct location!

---
+++ incursio-project/Incursio/Entities/AI/DefensiveAI.cs
@@ -0,0 +1,56 @@
+/****************************************
+ * Copyright © 2008, Team RobotNinja:
+ * 
+ *     - Henry Armstrong
+ *     - Andy Burras
+ *     - Mitch Martin
+ *     - Xuan Yu
+ * 
+ * All Rights Reserved
+ ***************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Incursio.Utils;
+using Incursio.Managers;
+using Incursio.Players;
+
+namespace Incursio.Entities.AI
+{
+    /// <summary>
+    /// DefensiveAI is a very basic, but functional, reactive artificial intelligence architecture.
+    /// 
+    /// AIPlayers with this AI will actively build an army and defenses, but will not actively hunt the player.
+    /// This AI will also act as a BaseAI
+    /// </summary>
+    public class DefensiveAI : BaseAI
+    {
+        public DefensiveAI(){
+
+        }
+
+        /// <summary>
+        /// This is where all AI actions are performed.  Actions are based off of this player's
+        /// event list; along with any other rules in sub-classes
+        /// </summary>
+        /// <param name="gameTime"></param>
+        /// <param name="player"></param>
+        public override void Update(Microsoft.Xna.Framework.GameTime gameTime, AIPlayer player){
+            //BUILD MY ARMY
+            //TODO: Improve - this currently just constantly builds light infantry!
+            /*EntityManager.getInstance().getLivePlayerStructures(player.id).ForEach(delegate(Structure s)
+            {
+                if(s.isConstructor){
+                    if( !(s as CampStructure).isBuilding() ){
+                        (s as CampStructure).build(new LightInfantryUnit());
+                    }
+                }
+            });
+            */
+            //TODO: CHECK EVENTS
+
+            base.Update(gameTime, player);
+        }
+    }
+}
